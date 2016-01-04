699f2d0778fc4e8eeaaa5830d762f71fa138a512
ddossant <ddossantos@gmail.com>
2016-01-04 13:02:32 -0800

    testing more

---
+++ src/test/helloworld.java
@@ -0,0 +1,20 @@
+/**
+ * Foothill Java 2 class practice notes
+ */
+
+/**
+ * @author dossant
+ * test 1 2
+ */
+public class Foothill {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow World!!!!");
+		System.out.println("more testing");
+	}
+
+}
